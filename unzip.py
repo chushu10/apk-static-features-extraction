2a5b872895e2ecf93a0d18fec250e7f764851cca
Chushu <liuchushu10@gmail.com>
2016-11-23 15:42:24 +0800

    unzip script

--- unzip.py
+++ unzip.py
@@ -5,5 +5,5 @@ import subprocess, os
 
 for filename in os.listdir('.'):
     if os.path.splitext(filename)[1] == '.7z'):
-        r = subprocess.call(['7z', 'x', filename+str(i)+'.7z', '-pinfected'])
-        os.remove(filename+str(i)+'.7z')
+        r = subprocess.call(['7z', 'x', filename, '-pinfected'])
+        os.remove(filename)
