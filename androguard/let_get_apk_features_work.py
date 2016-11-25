a09002fc585393ac98802a62f7310cc4d3ca1a07
Leo Choosue <liuchushu10@gmail.com>
2016-11-25 09:44:34 +0800

    update workflow

--- androguard/let_get_apk_features_work.py
+++ androguard/let_get_apk_features_work.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os, subprocess
+import os, subprocess, argparse
 
-directory = '/Users/liuchushu/Downloads/Dataset/vir-dataset'
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--directory', help='directory of the dataset')
+    args = parser.parse_args()
+    if args.directory:
+        directory = args.directory
+    else:
+        parser.print_help()
+
     for subdirectory in os.listdir(directory):
         if os.path.isdir(os.path.join(directory, subdirectory)):
             r = subprocess.call(['python', 'get_apk_features.py', '-d', os.path.join(directory, subdirectory), '-s', '-1'])
--- androguard/let_mass_static_analysis_work.py
+++ androguard/let_mass_static_analysis_work.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os, subprocess
+import os, subprocess, argparse
 
-directory = '/Users/liuchushu/Downloads/Dataset/vir-dataset'
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--directory', help='directory of the dataset')
+    args = parser.parse_args()
+    if args.directory:
+        directory = args.directory
+    else:
+        parser.print_help()
+
     for subdirectory in os.listdir(directory):
         if os.path.isdir(os.path.join(directory, subdirectory)):
             r = subprocess.call(['python', 'mass_static_analysis.py', '-d', os.path.join(directory, subdirectory)])
