52852736417a723675407679566eb22983c75630
Chushu <liuchushu10@gmail.com>
2016-12-06 15:45:41 +0800

    generate arff when finding incorrectly classified instances

--- tools/get_incorrectly_classified_instances.py
+++ tools/get_incorrectly_classified_instances.py
@@ -3,6 +3,83 @@
 
 import os, zipfile, argparse, arff
 
+obj = {
+    'description': u'',
+    'relation': 'testset',
+    'attributes': [
+        ('apk_size', 'REAL'),
+        ('dex_size', 'REAL'),
+        ('min_andrversion', 'INTEGER'),
+        ('max_andrversion', 'INTEGER'),
+        ('target_andrversion', 'INTEGER'),
+        ('security', ['-1','0']),
+        ('methodCount', 'INTEGER'),
+        ('classCount', 'INTEGER'),
+        ('crypto_count', 'INTEGER'),
+        ('dynCode_count', 'INTEGER'),
+        ('native_count', 'INTEGER'),
+        ('reflect_count', 'INTEGER'),
+        ('sendSMS', ['1', '0']),
+        ('deleteSMS', ['1', '0']),
+        ('interruptSMS', ['1', '0']),
+        ('httpPost', ['1', '0']),
+        ('deviceId', ['1', '0']),
+        ('simCountry', ['1', '0']),
+        ('installedPkg', ['1', '0']),
+        ('loadOtherCode', ['1', '0']),
+        ('subprocess', ['1', '0']),
+        ('executeOtherCode', ['1', '0']),
+        ('jni', ['1', '0']),
+        ('unix', ['1', '0']),
+        ('buttonCount', 'INTEGER'),
+        ('TextViewCount', 'INTEGER'),
+        ('EditViewCount', 'INTEGER'),
+        ('ImageButtonCount', 'INTEGER'),
+        ('CheckBoxCount', 'INTEGER'),
+        ('RadioGroupCount', 'INTEGER'),
+        ('RadioButtonCount', 'INTEGER'),
+        ('ToastCount', 'INTEGER'),
+        ('SpinnerCount', 'INTEGER'),
+        ('ListViewCount', 'INTEGER'),
+        ('fileCount', 'INTEGER'),
+        ('INTERNET', ['1', '0']),
+        ('SET_DEBUG_APP', ['1', '0']),
+        ('MODIFY_PHONE_STATE', ['1', '0']),
+        ('RECORD_AUDIO', ['1', '0']),
+        ('RECEIVE_BOOT_COMPLETED', ['1', '0']),
+        ('RECEIVE_MMS', ['1', '0']),
+        ('RECEIVE_SMS', ['1', '0']),
+        ('RECEIVE_WAP_PUSH', ['1', '0']),
+        ('SEND_SMS', ['1', '0']),
+        ('CALL_PHONE', ['1', '0']),
+        ('CALL_PRIVILEGED', ['1', '0']),
+        ('PROCESS_OUTGOING_CALLS', ['1', '0']),
+        ('READ_CALL_LOG', ['1', '0']),
+        ('READ_EXTERNAL_STORAGE', ['1', '0']),
+        ('READ_LOGS', ['1', '0']),
+        ('ACCESS_COARSE_LOCATION', ['1', '0']),
+        ('ACCESS_FINE_LOCATION', ['1', '0']),
+        ('BLUETOOTH', ['1', '0']),
+        ('CAMERA', ['1', '0']),
+        ('INSTALL_PACKAGES', ['1', '0']),
+        ('NFC', ['1', '0']),
+        ('READ_CONTACTS', ['1', '0']),
+        ('permissionCount', 'INTEGER'),
+        ('activityCount', 'INTEGER'),
+        ('serviceCount', 'INTEGER'),
+        ('receiverCount', 'INTEGER'),
+        ('providerCount', 'INTEGER'),
+        ('exportedCount', 'INTEGER'),
+        ('hPictureCount', 'INTEGER'),
+        ('mPictureCount', 'INTEGER'),
+        ('lPictureCount', 'INTEGER'),
+        ('xPictureCount', 'INTEGER'),
+        ('totalCount', 'INTEGER'),
+    ],
+    'data': [],
+}
+
+
 def get_size_and_dexsize(path_to_predictions, path_to_arff):
     # read predictions
     f = open(path_to_predictions, 'r')
@@ -24,7 +101,13 @@ def get_size_and_dexsize(path_to_predictions, path_to_arff):
     error_list = []
     i = 0
     for index in error_index:
+        obj['data'].append(d['data'][index])
         error_list.append({'size':d['data'][index][0], 'dex_size':d['data'][index][1]})
+
+    # write error vectors to arff
+    f = open('incorrectly_classified.arff', 'w')
+    arff.dump(obj, f)
+    f.close()
 
     return error_list
 
